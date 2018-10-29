9c4c99f6468e6b27cd5453e1c5dd0538928ed124
wowo <zhouyuexie@gmail.com>
2018-10-29 18:35:02 +0800

    temp: save

--- src/handles/rgb.rs
+++ src/handles/rgb.rs
@@ -4,9 +4,20 @@ use color::Color;
 
 // rgb -- rgba(81%,89%,12%,1),rgb(81,89,12),rgba(81%,89%,12%,0.3) etc..
 // return -- [0.81,0.89,0.12,1],[0.81,0.89,0.12] [0.81,0.89,0.12,0.3] etc...
-pub fn handle_rgb<'a>(color: &'a Color) -> Captures<'a> {
-	let re = Regex::new(r"(\d{1,3}\.?\d*%?),(\d{1,3}\.?\d*%?),(\d{1,3}\.?\d*%?),?(\d{1,3}\.?\d*%?)?").unwrap();
+pub fn handle_rgb<'a>(color: &'a Color) -> Result<Captures<'a>, String> {
+	let re = Regex::new(r"rgba?\((?P<r>\d{1,3}\.?\d*%?),(?P<g>\d{1,3}\.?\d*%?),(?P<b>\d{1,3}\.?\d*%?),?(?P<alpha>\d{1,3}\.?\d*%?)?\)").unwrap();
+	let cap = re.captures(color.to_str());
 
-	let cap: Captures = re.captures(color.to_str()).unwrap();
-	cap
+	if let Some(value) = cap {
+		Ok(value)
+	} else {
+		Err(format!("[color-convert] noting to match: {}", color.to_str()))
+	}
+}
+
+pub fn to_hsl(color: &Color) -> Result<Vec<f32>, String> {
+	let cap = handle_rgb(&color)?;
+	println!("{:?}", cap);
+
+	Ok(vec![3f32])
 }
--- tests/rgb.rs
+++ tests/rgb.rs
@@ -7,10 +7,22 @@ mod tests {
 	use color_convert::handles::rgb;
 //	use color_convert::utils;
 
+//	#[test]
+//	fn test_rgb_handle() {
+//		let color = Color::init("rgba");
+//		let cap = rgb::handle_rgb(&color);
+//
+//		if let Captures(value) = cap {
+//			println!("{:?}", value);
+//		} else {
+//			println!("noting");
+//		}
+//	}
+
 	#[test]
-	fn test_rgb_handle() {
-		let color = Color::init("rgba(81%,89%,12%,1)");
-		let cap = rgb::handle_rgb(&color);
-		println!("{:?}", cap);
+	fn test_rgb_tohsl() {
+		let color = Color::init("rgb(81%,89%,12%,1)");
+		let rgb_vec = rgb::to_hsl(&color);
+		println!("{:?}", rgb_vec);
 	}
 }
